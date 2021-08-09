9be5c437ab0712ed37e164f525a0692d2f091b76
Ben Faerber <benfaerber@protonmail.com>
2021-08-09 12:44:56 -0600

    Update server.py

--- server-flask/server.py
+++ server-flask/server.py
@@ -1,45 +1,56 @@
 import time
 import math
 from flask import Flask
+from flask import Response
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 port = 7438
 
+
 @app.route('/')
 def homepage():
-  homepage = '''
+    homepage = '''
   <h1>Theme Kit Hot Reload Server</h1>
   <a href="/notify">/notify</a> - Ping when a file has been updated or when the content was last acquired.<br>
   <a href="/updated">/updated</a> - An epoch timestamp indicating when the content was last updated.
   '''
-  return homepage
+    return homepage
+
 
 @app.route('/notify', methods=['get', 'post'])
 def notify():
-  epoch = math.floor(time.time())
-  handle = open('timestamp.txt', 'w')
-  handle.write(str(epoch))
-  handle.close()
-  return str(epoch)
+    epoch = math.floor(time.time())
+    print(f'Notified @ {str(epoch)}')
+    handle = open('timestamp.txt', 'w')
+    handle.write(str(epoch))
+    handle.close()
+    return str(epoch)
+
 
 @app.route('/updated')
 def updated():
-  handle = open('timestamp.txt', 'r')
-  epoch = handle.read()
-  handle.close()
-  return epoch
+    handle = open('timestamp.txt', 'r')
+    epoch = handle.read()
+    handle.close()
+    return epoch
+
+
+@app.route('/hotreload.js')
+def hotreload():
+    handle = open('hotreload.js')
+    source = handle.read()
+    handle.close()
+    return Response(source, mimetype='text/javascript')
+
 
 if __name__ == "__main__":
-  from waitress import serve
+    from waitress import serve
 
-  intro = f'''
-  Theme Kit Hot Reload Flask Server
-   - Home Page           @ http://localhost:{port}/
-   - Notify Change       @ http://localhost:{port}/notify
-   - Check Last Updated  @ http://localhost:{port}/updated
-  '''
-  print(intro)
+    intro = f'''Theme Kit Hot Reload Flask Server
+---------------------------------
+https://github.com/benfaerber/theme-kit-hot-reload'''
+    print(intro)
 
-  serve(app, host='0.0.0.0', port=port)
+    serve(app, host='0.0.0.0', port=port)
